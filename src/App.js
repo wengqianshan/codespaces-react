e47bc76205183f79c90eb2c6abe517a11e24975a
前山 <wengqianshan@me.com>
2022-12-25 08:23:11 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,27 +1,83 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // 商品列表
+  const items = [
+    {
+      id: 1,
+      name: '香蕉',
+      price: 5.99,
+    },
+    {
+      id: 2,
+      name: '橘子',
+      price: 3.2,
+    },
+    {
+      id: 3,
+      name: '梨',
+      price: 2,
+    },
+    {
+      id: 4,
+      name: '猕猴桃',
+      price: 8.8,
+    },
+    {
+      id: 5,
+      name: '柿子',
+      price: 5.0,
+    },
+  ];
+
+  // 记录选中项
+  const [checked, setChecked] = useState([]);
+
+  // 选中/反选点击
+  const handleClick = ({ id }) => {
+    const isChecked = checked.includes(id);
+    if (isChecked) {
+      const res = checked.filter((item) => item !== id);
+      setChecked(res);
+    } else {
+      setChecked([...checked, id]);
+    }
+  };
+
+  //  判断是否全选
+  const checkall = items.length === checked.length;
+
+  // 统计总金额
+  const total = checked.reduce((prev, curr) => {
+    const item = items.find((item) => item.id === curr);
+    const { price } = item;
+    return prev + price;
+  }, 0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className='list'>
+      {items.map((item, index) => {
+        const isChecked = checked.includes(item.id);
+        return (
+          <div
+            key={index}
+            className='item'
+            onClick={() => {
+              handleClick(item);
+            }}
           >
-            Learn React
-          </a>
-        </p>
-      </header>
+            <div>
+              {item.id}: {item.name}
+            </div>
+            {isChecked && <div>✅</div>}
+          </div>
+        );
+      })}
+      <div className='bar'>
+        <div>是否全选: {checkall ? '是' : '否'}</div>
+        <div>总金额: {total.toFixed(2)}</div>
+      </div>
     </div>
   );
 }
